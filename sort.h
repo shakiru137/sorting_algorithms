945420b97c922a676f770783f7a4bd1c74e3cc48
oluwasegun1379 <yusufshakiruoluwasegun137@gmail.com>
2024-03-04 17:55:17 +0100

    updated

--- sort.h
+++ sort.h
@@ -2,6 +2,9 @@
 #define SORT_H
 
 #include <stdio.h>
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -21,5 +24,55 @@ void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
 
+/**
+ * enum kind_e - Represents the different kinds of playing cards.
+ * @SPADE: Represents a spade card.
+ * @HEART: Represents a heart card.
+ * @CLUB: Represents a club card.
+ * @DIAMOND: Represents a diamond card.
+ */`
+typedef enum kind_e
+{
+	SPADE = 0,
+	HEART,
+	CLUB,
+	DIAMOND
+} kind_t;
+
+/**
+ * struct card_s - Playing card
+ *
+ * @value: Value of the card
+ * From "Ace" to "King"
+ * @kind: Kind of the card
+ */
+typedef struct card_s
+{
+	const char *value;
+	const kind_t kind;
+} card_t;
+
+/**
+ * struct deck_node_s - Deck of card
+ *
+ * @card: Pointer to the card of the node
+ * @prev: Pointer to the previous node of the list
+ * @next: Pointer to the next node of the list
+ */
+typedef struct deck_node_s
+{
+	const card_t *card;
+	struct deck_node_s *prev;
+	struct deck_node_s *next;
+} deck_node_t;
 #endif /* SORT_H */
